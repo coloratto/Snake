3bd177ab9832aaba2214e7d9254a252b88d8c83a
unknown <sdevireddy@6X0SFV1.mi.corp.rockfin.com>
2014-06-05 16:45:28 -0400

    Made some small CSS changes and started the JS

---
+++ snake.js
@@ -0,0 +1,50 @@
+var left=37;
+var up=38;
+var right=39;
+var down=40;
+var field;
+var snake;
+var currentScore = 0;
+var gameSpeed = 1
+
+function field()
+{
+	this.drawSomething = function(class, x, y)
+	{
+
+	}
+}
+
+
+function snake() {
+	var length = 1;
+	//this.blah = function() {
+	//	...
+	//};
+	//or
+	//this.blahblah = dkjfskd;
+}
+
+/*function drawSnake() {
+	for(var i=0; i<snake.length; ++i)
+	{
+		//field.drawElement('bo')
+	}
+}*/
+
+function initializeGame() {
+	endPreviousGame();
+	field = new field();
+	snake = new snake();
+	currentScore = 0;
+	var directionToMove = right;
+	drawSnake(snake);
+};
+
+$(document).ready(function() {
+	initializeGame();
+	$(document).keydown(function(key) {
+		directionToMove = parseIn(key.which, 10);
+		moveInDirection(directionToMove);
+	});
+});
